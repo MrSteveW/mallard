bf885eba18f16c967223fd23a6ddf39d8cf3e749
MrSteveW <MrSteveW@users.noreply.github.com>
2026-04-19 17:10:58 +0100

    fix: DutyCalendar resource time HH:MM

--- app/Http/Resources/DutyCalendarResource.php
+++ app/Http/Resources/DutyCalendarResource.php
@@ -14,18 +14,21 @@ class DutyCalendarResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $startTime = substr($this->start_time, 0, 5);
+        $endTime = substr($this->end_time, 0, 5);
+
         return [
             'id' => $this->id,
             'title' => $this->user->name,
-            'start' => $this->date.'T'.$this->start_time.':00',
-            'end' => $this->end_time < $this->start_time
+            'start' => $this->date.'T'.$startTime.':00',
+            'end' => $endTime < $startTime
                 ? $this->date.'T23:59:00'
-                : $this->date.'T'.$this->end_time.':00',
+                : $this->date.'T'.$endTime.':00',
             'extendedProps' => [
                 'user_id' => $this->user_id,
                 'shift_type' => $this->shift_type,
-                'start_time' => $this->start_time,
-                'end_time' => $this->end_time,
+                'start_time' => $startTime,
+                'end_time' => $endTime,
                 'notes' => $this->notes,
                 'grade' => $this->user->employee?->grade?->name ?? '',
                 'cancelled_at' => $this->cancelled_at,
